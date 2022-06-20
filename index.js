4ab9aa91fc92b2531dd3a1e1a84e7ab75c15503d
sharonibejih <sharonibejih@gmail.com>
2022-06-20 21:39:28 +0100

    used js to write texts on screen

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Name: Sharon Ibejih";
+document.getElementById("height").innerHTML = 'Height: 5.67 " ';
+document.getElementById("country").innerHTML = "Country: Nigeria";
